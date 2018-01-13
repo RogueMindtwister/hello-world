b704ebc76784a59807af73b8059bb94fc7aae090
Alexander White <lordalex14@gmail.com>
2018-01-12 21:13:22 -0500

    Added code
    Added more code to the program

--- test.cpp
+++ test.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void displayText(){
+    cout << "Displaying text..." << endl;
+    cout << "This text is amazing. <3" << endl;
+}
+
 int main(){
     cout << "Hello World!" << endl;
+    for (int i = 0; i < 100; i++){
+        cout << "Value of i: " << i << endl;
+    }
+       
+    displayText();
     
     cout << "Press ENTER to continue...";
     cin.get();
